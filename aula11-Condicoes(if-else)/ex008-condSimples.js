e03c965ac98f0a3411973ae9e802ba0e8246d8e1
Mike Linder <miky2493@gmail.com>
2022-09-29 15:50:04 -0300

    iniciando consições compostas

---
+++ aula11-Condicoes(if-else)/ex008-condSimples.js
@@ -0,0 +1,8 @@
+//Condição simples (if)
+
+var vel = 20
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { 
+    console.log('Você ultrapassou o limite de velocidade, FOI MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
